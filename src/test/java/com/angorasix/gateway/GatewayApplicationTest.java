980fc3bd04afbfb356ce8e74855b70b936559958
Ger Roza <rozagerardo@gmail.com>
2021-11-07 22:52:06 -0300

    removed coverage limits temporarily and fixed test

--- src/test/java/com/angorasix/gateway/GatewayApplicationTest.java
+++ src/test/java/com/angorasix/gateway/GatewayApplicationTest.java
@@ -2,8 +2,13 @@ package com.angorasix.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {
+        "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:${wiremock.server.port}",
+    })
+@AutoConfigureWireMock(port = 0, stubs = "classpath:/stubs/configs/init", files = "classpath:/stubs")
 public class GatewayApplicationTest {
 
   @Test
